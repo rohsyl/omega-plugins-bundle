30279c2b217f8b7052dc935cfd71108c9046d7a6
huser <huser.gaetan@hotmail.com>
2021-12-20 13:26:58 +0100

    basic display text and image

--- resources/views/overt/text_and_image/display.blade.php
+++ resources/views/overt/text_and_image/display.blade.php
@@ -1,60 +1,8 @@
 <div class="">
-    @php
-        if(!function_exists('textandimage_display_text')){
-            function textandimage_display_text($text) {
-                echo '<div class="">';
-                echo $text;
-                echo '</div>';
-            }
-        }
-        if(!function_exists('textandimage_display_image')){
-            function textandimage_display_image($picture, $resize, $w, $h) {
-                if(isset($picture)){
-                    echo '<div class="">';
-                    if($resize) {
-                        echo '<img src="'.$picture->GetThumbnail($w, $h).'" alt="'.$picture->name.'" width="100%" />';
-                    }
-                    else{
-                        echo '<img src="'.asset($picture->path).'" alt="'.$picture->name.'" width="100%" />';
-                    }
-                    echo '</div>';
-                }
-            }
-        }
-        $image_width = $width_percent['value'];
-        $position = $position['value'];
-        $resize = $resize['resize'];
-        $LEFT = 0;
-        $RIGHT = 1;
-        $TOP = 2;
-        $BOTTOM = 3;
-        $width = intval($width);
-        $height = intval($height);
-    @endphp
-
-    @if($position == $TOP)
-        {!! textandimage_display_image($picture, $resize, $width, $height) !!}
-        {!! textandimage_display_text($text) !!}
-    @elseif($position == $BOTTOM)
-        {!! textandimage_display_text($text) !!}
-        {!! textandimage_display_image($picture,$resize, $width, $height) !!}
-    @elseif($position == $LEFT)
-        <div class="row">
-            <div class="col-sm-{{ $image_width }}">
-                {!! textandimage_display_image($picture, $resize, $width, $height) !!}
-            </div>
-            <div class="col-sm-{{ 12 - $image_width }}">
-                {!! textandimage_display_text($text) !!}
-            </div>
-        </div>
-    @elseif($position == $RIGHT)
-        <div class="row">
-            <div class="col-sm-{{ 12 - $image_width }}">
-                {!! textandimage_display_text($text) !!}
-            </div>
-            <div class="col-sm-{{ $image_width }}">
-                {!! textandimage_display_image($picture, $resize, $width, $height) !!}
-            </div>
+    {!! @$text !!}
+    @if(isset($url))
+        <div>
+            <img src="{{ $url->url }}" title="{{ $url->name }}" width="100%" />
         </div>
     @endif
 </div>
--- src/Plugins/PluginTextAndImage.php
+++ src/Plugins/PluginTextAndImage.php
@@ -6,6 +6,7 @@ use rohsyl\OmegaCore\Utils\Common\Plugin\Form\PluginFormFactory;
 use rohsyl\OmegaCore\Utils\Common\Plugin\Plugin;
 use rohsyl\OmegaCore\Utils\Common\Plugin\Type\TextRich\TextRich;
 use rohsyl\OmegaCore\Utils\Common\Plugin\Type\MediaChooser\MediaChooser;
+use rohsyl\OmegaCore\Utils\Common\Plugin\Type\DropDown\DropDown;
 use rohsyl\OmegaPlugin\Bundle\Http\Controllers\Overt\TextAndImage\PluginController;
 
 
@@ -35,10 +36,20 @@ class PluginTextAndImage extends Plugin
             'preview' => true,
             'type' => [Media::MT_PICTURE]
         ];
-        $this->makeForm(function(PluginFormFactory $builder) use ($param) {
+        $positionParam = [
+            "default" => 1,
+            "options" => [
+                "1" => "Top",
+                "2" => "Right",
+                "3" => "Bottom",
+                "4" => "Left"
+            ]
+        ];
+        $this->makeForm(function(PluginFormFactory $builder) use ($param, $positionParam) {
             $builder->form('TextAndImage', true, true);
             $builder->entry('text', TextRich::class, null, 'Text', null, 0, false);
             $builder->entry('url', MediaChooser::class, $param, 'Image', null, 0, false);
+            $builder->entry('level', DropDown::class, $positionParam, 'Position', 'The position of the image to the text', 2, false);
         });
 
     }
